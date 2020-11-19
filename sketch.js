3443463e35571c21a3f7c2e77b89f1e82a78758a
jacquelinelouie <jacquelinelouie@users.noreply.github.com>
2020-11-19 11:15:14 -0800

    giphy API import

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+var api = "http://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=q2PtZp2vyGmiuCZPkC6zGIiIQzOtKieq";
+var query = "&q=rainbow";
+//rainbow not hardcoded into code, want to get from user input AKA search result
+
+function setup(){
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+  //p5 function, give url from API and get JSON
+}
+
+function gotData(giphy){
+  for (var i = 0; i < giphy.data.length; i++){
+  createImg(giphy.data[0].images.original.url);
+  //source of image tag
+  }
+}
+function draw(){
+
+}
